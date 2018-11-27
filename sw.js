94f29e00ea631e2a18caaf40e599acee7f651394
LoongkoLeo <zdzz1091@gmail.com>
2018-11-27 14:18:39 +0800

    Remake

--- sw.js
+++ sw.js
@@ -107,18 +107,19 @@
             maxEntries: maxEntries
         }
     });
-
+    
     self.toolbox.router.get('/*', self.toolbox.networkFirst, {
         cache: {
             name: contentCacheName,
             maxEntries: maxEntries
         }
     });
-
+    
     self.addEventListener("install", function(event) {
         return event.waitUntil(self.skipWaiting())
     });
     self.addEventListener("activate", function(event) {
         return event.waitUntil(self.clients.claim())
     })
-})();
+}
+)();
